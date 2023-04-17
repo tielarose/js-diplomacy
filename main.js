f8a78841815992d667afd80b8a827594b1392d84
Tiela Black-Law <tiela.rose@gmail.com>
2023-04-17 10:41:05 -0700

    Add changeColor function

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+
+function changeColor() {
+  const itemsWithClassColorChange = document.querySelectorAll(".color-change");
+
+  for (const item of itemsWithClassColorChange) {
+    item.classList.add("red");
+  }
+}
+
+document.querySelector(".color-changer").addEventListener("click", changeColor);
