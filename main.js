d06abd5251e3a5f0d42ee2ba5075770cef89cfcd
Tiela Black-Law <tiela.rose@gmail.com>
2023-04-17 11:09:15 -0700

    Complete validateInput function

--- main.js
+++ main.js
@@ -8,4 +8,21 @@ function changeColor() {
   }
 }
 
+function validateInput(evt) {
+  evt.preventDefault();
+
+  const numInput = document.querySelector("#number-input");
+  const userInput = Number(numInput.value);
+
+  const validationMsg = document.querySelector("#formFeedback");
+  if (isNaN(userInput || userInput >= 10)) {
+    validationMsg.innerHTML = "Please enter a small number";
+  } else {
+    validationMsg.innerHTML = "You are good to go!";
+  }
+}
+
 document.querySelector(".color-changer").addEventListener("click", changeColor);
+document
+  .querySelector("[type='submit']")
+  .addEventListener("click", validateInput);
